9d7a867c2b691dc7f6a7aae411232cdd0993791b
Harsh <harsh97830544@gmail.com>
2025-09-07 04:43:06 +0000

    sum of two digit number

---
+++ sum_of_two_digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{  
+    int a,b,sum;
+         printf("Enter 1st number: ");
+         scanf("%d",&a);
+    
+         printf("Enter 2nd number: ");
+         scanf("%d",&b);
+
+      sum=a+b;
+
+    printf("Sum of %d and %d is %d\n",a,b,sum);
+    return 0;
+}

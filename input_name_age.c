c90e56280b1ed5c33ad7cfb1c7e5f9576609e2d6
Harsh <harsh97830544@gmail.com>
2025-09-07 04:41:16 +0000

    input age and name and print name with age

---
+++ input_name_age.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char name[10];
+int age ;
+ printf("enter name :");
+    scanf("%s",name);
+printf("enter age :");
+ scanf("%d",&age);
+ printf("name is %s \n age is %d\n",name,age);
+
+    return 0;
+
+}

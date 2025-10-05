97210dd58e4ab3af4c32679aac273a7ee30bab9f
Harsh <harsh97830544@gmail.com>
2025-10-05 10:25:34 +0000

    arary1

---
+++ array1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int largest, second_largest;
+    if (arr[0] > arr[1])
+    {
+        largest = arr[0];
+        second_largest = arr[1];
+    }
+    else
+    {
+        largest = arr[1];
+        second_largest = arr[0];
+    }
+
+    for (i = 2; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            second_largest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > second_largest && arr[i] != largest)
+        {
+            second_largest = arr[i];
+        }
+    }
+
+    printf("Second largest integer: %d\n", second_largest);
+    return 0;
+}
